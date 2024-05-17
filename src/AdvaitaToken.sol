c19da6e0cae46f8245bf864e21c3830b5308c20a
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-17 21:16:41 +0530

    create Token using openzepplin

---
+++ src/AdvaitaToken.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.18;
+
+import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract AdvaitaToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("AdvaitaToken", "ADV") {
+        _mint(msg.sender, initialSupply);
+    }
+}
